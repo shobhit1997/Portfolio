222e0e78ce44cf93c84291c7f5c4c4e887b4b58e
shobhit1997 <shobhit.shobhitagarwal.agarwal@gmail.com>
2020-02-29 12:40:29 +0530

    update

--- src/components/experienceComponent.vue
+++ src/components/experienceComponent.vue
@@ -3,10 +3,21 @@
 		<ul style="list-style:none;margin:10px 0;">
 			<li>
 				<div>
+					<h3><a href="https://www.softwareag.com/">SoftwareAG, Bangalore</a></h3>
+					<hr>
+					<h4><b>R&D Intern (January,2020-Present)</b></h4>
+					<ul>
+						<li>Working on Integration Cloud and WebMethods.io</li>
+						<li>Building Connectors on Cloud Streams</li>
+						<li>Working on Saas Platforms and Java.</li>
+					</ul>
+				</div>
+			</li>
+			<li>
+				<div>
 					<h3><a href="https://www.gradeup.co/">GradeUp, Noida</a></h3>
 					<hr>
 					<h4><b>Backend Intern (June,2019-September,19)</b></h4>
-					<h4>Intern</h4>
 					<ul>
 						<li>Building API’s on Node.js for Ed-Tech product</li>
 						<li>Writing Resolvers on GraphQL</li>
@@ -20,7 +31,6 @@
 					<h3><a href="https://www.lecupido.com/">Cupido (Startup), Bangalore</a></h3>
 					<hr>
 					<h4><b>Backend Intern (April,2019-May,19)</b></h4>
-					<h4>Intern</h4>
 					<ul>
 						<li>Building API’s on Node.js for E-Commerce Product</li>
 						<li>Converting business logic to code</li>
@@ -33,7 +43,6 @@
 					<h3><a href="http://www.zester.in">Zester (Startup), Noida</a></h3>
 					<hr>
 					<h4><b>Rest Api development on Node.js (June,2018-December,18)</b></h4>
-					<h4>Intern</h4>
 					<ul>
 						<li>Building API’s on Node.js for web and mobile apps for Social Networking platform</li>
 						<li>Solving day to day bugs on the live website</li>
@@ -46,7 +55,6 @@
 					<h3><a href="http://www.hopaka.com">Hopaka Technologies (Startup), Noida</a></h3>
 					<hr>
 					<h4><b>Android App Development (Feb,2018-April,2018)</b></h4>
-					<h4>Intern</h4>
 					<p>Developed an android app to solve parking problems in cities.</p>
 				</div>
 			</li>
@@ -55,7 +63,6 @@
 					<h3><a href="https://www.eninov.com">Eninov Systems, Noida</a></h3>
 					<hr>
 					<h4><b>Hybrid App Development (June,2017-August,2017)</b></h4>
-					<h4>Intern</h4>
 					<p>Worked on an accounting app based on phonegap for the organisation.</p>
 				</div>
 			</li>
@@ -64,7 +71,6 @@
 					<h3><a href="http://www.expertsinstitute.co.in/">Experts' Institute, Allahabad</a></h3>
 					<hr>
 					<h4><b>Java Teaching (April,2016-June,2016)</b></h4>
-					<h4>Intern</h4>
 					<p>Teaching core java to class 9th-10th students</p>
 				</div>
 			</li>
